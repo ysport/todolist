21a35b2a13143e7af2fae69946109630d81171dd
Sammy <ksamy90@aol.com>
2019-11-28 12:37:14 +0300

    todolist app

---
+++ src/components/NewTodoForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import uuid from "uuid";
+
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.createTodo({ ...this.state, id: uuid() });
+    this.setState({ task: "" });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>New Todo</label>
+        <input
+          type="text"
+          placeholder="New todo"
+          id="task"
+          name="task"
+          value={this.state.task}
+          onChange={this.handleChange}
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
